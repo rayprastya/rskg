afc65770bebf2a10e93ee2ac730f48f2bf84c1ed
unknown <rayhan.prastya@gmail.com>
2020-09-02 16:42:53 +0700

    update cek antrian

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import rskg
 
-run=rskg.rskgi()
+print("Pendaftaran Layanan Rumah Sakit")
+print("Silahkan pilih layanan yang ingin digunakan")
+print("1. Antrian Hemodialisa")
+print("2. Antrian Poliklinik")
+print("( Masukan angka nya saja )")
+
+antrian=input()
+
+run=rskg.rskgi(antrian)
 run.login()
+#run.ambil_antrian()
+run.cek_kamar()
--- rskg.py
+++ rskg.py
@@ -7,11 +7,12 @@ from time import sleep
 import os
 
 class rskgi(object):
-    def __init__(self):
+    def __init__(self,antrian):
         self.driver = webdriver.Chrome()
-
-    def login(self):
-        self.driver.get('http://192.168.55.127/apprskgteaminova/index.php?r=site/login')
+        self.antrian =  antrian
+        
+    def login(self):#http://192.168.55.127/apprskgteaminova/index.php?r=site/login
+        self.driver.get('http://192.168.55.127/ehospitalrskg/index.php?r=site/login')
         self.driver.find_element_by_id('LoginForm_username').send_keys('admin')
         sleep(2)
         self.driver.find_element_by_id('LoginForm_password').send_keys('inovabdg')
@@ -25,8 +26,35 @@ class rskgi(object):
         self.driver.find_element_by_css_selector("#LoginForm_ruangan [value='1']").click()
         sleep(1)
         self.driver.find_element_by_name('yt0').click()
-        sleep(1)
-        self.driver.find_element_by_class_name('close').click()
+        sleep(2)
+        self.driver.find_element_by_xpath('//*[@id="yw0"]/div/a').click()
+
+    def ambil_antrian(self):
         self.driver.find_element_by_class_name('shortcut4').click()
-        self.driver.find_element_by_class_name('shortcut').click()
-        self.driver.find_element_by_name('yt1').click()
+        self.driver.find_element_by_xpath('//*[@id="content"]/div/div/div/a[1]').click()
+
+        if self.antrian == "1":
+            self.driver.find_element_by_name('yt0').click()
+            print("")
+            print("anda telah terdaftar dalam antrian hemodialisa")
+
+        elif self.antrian == "2":
+            self.driver.find_element_by_name('yt1').click()
+            print("")
+            print("anda telah terdaftar dalam antrian poliklinik")
+
+        else:
+            print("maaf pilihan antrian anda tidak terdaftar disini")
+    
+    def cek_kamar(self):
+        self.driver.find_element_by_class_name('shortcut4').click()
+        self.driver.find_element_by_xpath('//*[@id="content"]/div/div/div/a[2]').click()
+
+        count = 0
+        int(nilai) = self.driver.find_element_by_class_name("no-antrian")
+        for x in int(count):
+            if count < nilai:
+                count.append(nilai)
+                continue
+            else:
+                break
